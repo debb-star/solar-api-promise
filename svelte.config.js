69f4aa8e9095ce4f6d0f2f7a342de6cdf7af6a2c
debb-star <debangan@fractionally.co>
2024-11-27 10:50:50 +0530

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,3 @@
-/*
- Copyright 2023 Google LLC
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      https://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import * as child_process from 'node:child_process';
@@ -29,6 +13,11 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+
+    // Add the prerender configuration
+    prerender: {
+      handleMissingId: 'warn', // Options: 'ignore', 'warn', 'error'. Default is 'error'.
+    },
 
     // https://kit.svelte.dev/docs/configuration#version
     version: {
